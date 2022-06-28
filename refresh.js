ba6863cb509d3b9f0dcab4f043bba6089840133c
Joey Marcinowski <66542880+Pixels128@users.noreply.github.com>
2022-06-28 18:53:56 -0400

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,13 +1,21 @@
 /* 
+
 1. Add the script tag below to in the head of HTML:
+
     <script src="https://combinatronics.com/Pixels128/Toolkit/main/refresh.js"></script>
+
 2. Inside a script tag in the body, call the function: 
+
     refresh("/path-to-your-api")
+
 3. replace `path-to-your-api` to an API endpoint, which returns JSON in the form of 
+
     {
         "state": unique-int-or-string
     }
+
 4. `unique-int-or-string` should change only when the application is restarted.
+
 */
 
 let initalState = 0
